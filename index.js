d610077044cd77b62a319d7b281dfd751fe15a28
Franz Sittampalam <franz.sittampalam@natwestmarkets.com>
2020-02-24 12:57:11 +0000

    update script as per https://github.com/avajs/typescript

--- index.js
+++ index.js
@@ -1,61 +1,26 @@
-// Mrm module to work with new line separated text files
-const {
-  // JSON files
-  json,
-  // package.json
-  packageJson,
-  // New line separated text files
-  lines,
-  // Install npm packages
-  install
-} = require('mrm-core');
+const { json, packageJson, lines, install } = require("mrm-core");
 
 function task() {
-   const file = json('./package.json');
-   file.exists()
-   install('ts-node')
-   install('esm')
-   install('ava')
-   install('@babel/register')
-   install('@babel/core')
-   install('@babel/polyfill')
-   install('@ava/babel-preset-stage-4')
-   install('typescript')
-   install('@types/node')
-   const pkg = packageJson()
-    .setScript('test', 'ava')
-    .setScript('test:watch', 'ava --watch')
-    .save()
-   file.merge({
-       "ava": {
-          "compileEnhancements": false,
-          "extensions": [ "ts"],
-          "require": [
-            "esm",
-            "ts-node/register",
-            "@babel/register",
-            "@babel/polyfill",
-          ],
-          "babel": {
-          "testOptions": {
-            "plugins": [
-              "@babel/plugin-syntax-jsx"
-            ],
-            "presets": [
-              "module:ava/stage-4",
-              {
-                "modules": false
-              }
-            ],
-            "extensions": [
-              "ts"
-            ]
-          }
+  const file = json("./package.json");
+  file.exists();
+  install("@ava/typescript");
+  install("ava");
+  const pkg = packageJson()
+    .setScript("test", "ava")
+    .setScript("test:watch", "ava --watch")
+    .save();
+  file.merge({
+    ava: {
+      typescript: {
+        extensions: ["ts", "tsx"],
+        rewritePaths: {
+          "src/": "dist/"
         }
-        }
-      })
-   file.save();
+      }
+    }
+  });
+  file.save();
 }
 
-task.description = 'Adds ava config';
+task.description = "Adds ava config";
 module.exports = task;
